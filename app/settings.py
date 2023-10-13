683d728beb9e39b8afad8fb4b7d3a3c016b0a928
Herco ZauZau <zauzauherco@gmail.com>
2023-10-12 19:23:34 -1200

    update static path

--- app/settings.py
+++ app/settings.py
@@ -104,7 +104,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static/'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static/app'),)
 STATIC_ROOT = os.path.join('static')
 
 
